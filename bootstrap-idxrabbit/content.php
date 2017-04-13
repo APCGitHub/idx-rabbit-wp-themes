88540fc93dfc3053e0272bd99bd3277e433c7d8a
Andrew Clinton <ddrew.clinton@gmail.com>
2017-04-13 15:53:29 -0400

    fix layout and content issues

--- bootstrap-idxrabbit/content.php
+++ bootstrap-idxrabbit/content.php
@@ -1,7 +1,12 @@
 <div class="blog-post">
-	<h2 class="blog-post-title"><?php the_title(); ?></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+	 <h2 class="blog-post-title"><?php the_title(); ?></h2>
 
- <?php the_content(); ?>
+	 <?php if(!is_page()): ?>
+	 	<p class="blog-post-meta">
+	 		<?php the_date(); ?> by <a href="#"><?php the_author(); ?></a>
+	 	</p>
+	 <?php endif; ?>
+
+ 	<?php the_content(); ?>
 
 </div><!-- /.blog-post -->
--- bootstrap-idxrabbit/index.php
+++ bootstrap-idxrabbit/index.php
@@ -2,7 +2,13 @@
 
 <div class="row">
 	<div class="col-sm-12 col-md-8">
-		<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php 
+			if ( have_posts() ) : while ( have_posts() ) : the_post();
+	
+				get_template_part( 'content', get_post_format() );
+
+			endwhile; endif; 
+		?>
 	</div> <!-- /.main -->
 	<div class="col-sm-12 col-md-4">
 		<?php get_sidebar(); ?>
