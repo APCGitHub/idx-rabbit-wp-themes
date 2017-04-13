2e3939336d46eac2c6011498f4750cf08d272ff3
Andrew Clinton <ddrew.clinton@gmail.com>
2017-04-13 15:52:19 -0400

    update functions

--- bootstrap-idxrabbit/functions.php
+++ bootstrap-idxrabbit/functions.php
@@ -1,4 +1,7 @@
 <?php
 
-define('IDXRABBIT_CHILD_BASE_DIRECTORY', get_stylesheet_directory());
-define('IDXRABBIT_CHILD_BASE_DIRECTORY_URI', get_stylesheet_directory_uri());
+define('BOOTSTRAP_IDXRABBIT_BASE_DIRECTORY', get_stylesheet_directory());
+define('BOOTSTRAP_IDXRABBIT_BASE_DIRECTORY_URI', get_stylesheet_directory_uri());
+define('BOOTSTRAP_IDXRABBIT_CONFIG_DIRECTORY', get_template_directory() . '/config/');
+
+require BOOTSTRAP_IDXRABBIT_BASE_DIRECTORY . '/config/config.php';
