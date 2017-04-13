6518102c70610a68fb0625e98cdda3173f0e5385
Andrew Clinton <ddrew.clinton@gmail.com>
2017-04-13 16:38:09 -0400

    small syntax change

--- bootstrap-idxrabbit/sidebar.php
+++ bootstrap-idxrabbit/sidebar.php
@@ -1,25 +1,24 @@
-<?php if(is_home()): ?>
-	<?php
+<?php if(is_home()): 
 		echo do_shortcode("[render template='views/featured' top='3' layout='list']");
-	?>
+?>
 <?php else: ?>
-<div class="sidebar-module sidebar-module-inset">
-	<h4>About</h4>
-	<p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-</div>
-<div class="sidebar-module">
-	<h4>Archives</h4>
-	<ol class="list-unstyled">
-		<li><a href="#">March 2014</a></li>
-		<!-- More archive examples -->
-	</ol>
-</div>
-<div class="sidebar-module">
-	<h4>Elsewhere</h4>
-	<ol class="list-unstyled">
-		<li><a href="#">GitHub</a></li>
-		<li><a href="#">Twitter</a></li>
-		<li><a href="#">Facebook</a></li>
-	</ol>
-</div>
+	<div class="sidebar-module sidebar-module-inset">
+		<h4>About</h4>
+		<p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+	</div>
+	<div class="sidebar-module">
+		<h4>Archives</h4>
+		<ol class="list-unstyled">
+			<li><a href="#">March 2014</a></li>
+			<!-- More archive examples -->
+		</ol>
+	</div>
+	<div class="sidebar-module">
+		<h4>Elsewhere</h4>
+		<ol class="list-unstyled">
+			<li><a href="#">GitHub</a></li>
+			<li><a href="#">Twitter</a></li>
+			<li><a href="#">Facebook</a></li>
+		</ol>
+	</div>
 <?php endif; ?>
