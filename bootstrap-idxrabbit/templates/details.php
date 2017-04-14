7a8caa2ec958626567ca139bc3be3ae04028dcd3
Andrew Clinton <ddrew.clinton@gmail.com>
2017-04-14 16:58:33 -0400

    until we have the twig branch allowing direct wp function calling, pass in the current url

--- bootstrap-idxrabbit/templates/details.php
+++ bootstrap-idxrabbit/templates/details.php
@@ -8,8 +8,10 @@
 	<div class="col-sm-12">
 		<?php 
 			$listing_id = get_query_var('listing_id');
+			$actions = get_query_var('do');
+			$current_url = home_url('property-details') . '/' . $listing_id;
 
-			echo do_shortcode("[render template='views/details' listing_id='$listing_id']");
+			echo do_shortcode("[render template='views/details' listing_id='$listing_id' on_load='$actions' current_url='$current_url']");
 		 ?>
 	</div>
 </div>
