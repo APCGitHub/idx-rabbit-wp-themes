a62ba5c629a0e290e5d13d7cac54d760bac6e568
Andrew Clinton <ddrew.clinton@gmail.com>
2017-04-14 16:57:43 -0400

    detail component

---
+++ bootstrap-idxrabbit/resources/assets/js/components/details.js
@@ -0,0 +1,70 @@
+import Vue from 'vue';
+import axios from 'axios';
+import qs from 'qs';
+
+Vue.component('property-details', {
+	props: {
+		loadActions: {
+			type: Array
+		},
+		listing: {
+			type: Object,
+			required: true
+		},
+		currentUrl: {
+			required: true,
+			type: String
+		}
+	},
+	data() {
+		return {
+			http: {
+				favoriting: false
+			},
+			on_load_actions: this.loadActions,
+			actions: [
+				'favorite_listing'
+			],
+			action_map: {
+				favorite_listing: 'favoriteListing'
+			},
+			liked: null
+		};
+	},
+	created() {
+		for(var i = 0; i < this.on_load_actions.length; i++) {
+			let a = this.on_load_actions[i];
+
+			if(this.actions.indexOf(a) > -1) {
+				let method = this.getMethod(a);
+
+				if(method)
+					this[method]();
+			}
+		}
+	},
+	methods: {
+		getMethod(action) {
+			return this.action_map[action];
+		},
+		favoriteListing() {
+			this.http.favoriting = true;
+
+			this.$http.post('/wp-admin/admin-ajax.php', qs.stringify({
+				action: 'post_favorite_listing',
+				listing_id: this.listing.ListingId,
+				listing_url: this.currentUrl
+			})).then(res => {
+				let body = res.data;
+				if(body.success)
+					this.liked = true;
+
+				this.http.favoriting = false;
+			}).catch(err => {
+				let error = err.response.data;
+				this.http.favoriting = false;
+				console.log(error);
+			});
+		}
+	}
+});
