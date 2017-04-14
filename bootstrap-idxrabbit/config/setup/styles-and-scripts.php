f189b450677e7a4fde5048269b75bf6a5b347c5b
Andrew Clinton <ddrew.clinton@gmail.com>
2017-04-14 16:56:15 -0400

    so this was right, i just hadn’t used the footer tag -.-

--- bootstrap-idxrabbit/config/setup/styles-and-scripts.php
+++ bootstrap-idxrabbit/config/setup/styles-and-scripts.php
@@ -4,7 +4,7 @@ add_action('wp_enqueue_scripts', 'bootstrap_idxrabbit_frontend_scripts_and_style
 
 function bootstrap_idxrabbit_frontend_scripts_and_styles()
 {
-	wp_register_script('bootstrap-idxrabbit-theme-scripts', BOOTSTRAP_IDXRABBIT_BASE_DIRECTORY_URI . '/public/js/app.js', array('jquery'), '001');
+	wp_register_script('bootstrap-idxrabbit-theme-scripts', BOOTSTRAP_IDXRABBIT_BASE_DIRECTORY_URI . '/public/js/app.js', array('jquery'), '110', true);
 
 	wp_register_style('bootstrap-idxrabbit-theme-styles', BOOTSTRAP_IDXRABBIT_BASE_DIRECTORY_URI . '/public/css/app.css');
 
