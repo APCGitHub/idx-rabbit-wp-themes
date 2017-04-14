caebf9dacca960c284f6cbcc56d2d19e67883b56
Andrew Clinton <ddrew.clinton@gmail.com>
2017-04-14 16:56:00 -0400

    add app id to pages

--- bootstrap-idxrabbit/page.php
+++ bootstrap-idxrabbit/page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-	<div class="row">
+	<div class="row" id="idxrabbit-app">
 		<div class="col-sm-12">
 			<?php 
 				if ( have_posts() ) : while ( have_posts() ) : the_post();
