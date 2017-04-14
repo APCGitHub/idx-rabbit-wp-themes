e5734819a1d9bc337d1f79ed454d7e50ca08488f
Andrew Clinton <ddrew.clinton@gmail.com>
2017-04-14 14:15:23 -0400

    webpack update

--- bootstrap-idxrabbit/webpack.mix.js
+++ bootstrap-idxrabbit/webpack.mix.js
@@ -1,4 +1,9 @@
 const { mix } = require('laravel-mix');
 
+mix.options({ processCssUrls: false });
+
+//output assets in the public dir
+mix.setPublicPath('public');
+
 mix.sass('resources/assets/sass/app.scss', 'public/css')
 	.js('resources/assets/js/app.js', 'public/js');
