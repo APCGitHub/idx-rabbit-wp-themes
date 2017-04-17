663ea9fefa6a4ad5ab834dcf37e8fbc7b0a4d092
Andrew Clinton <ddrew.clinton@gmail.com>
2017-04-17 15:36:10 -0400

    results updates to use vue and other changes

--- bootstrap-idxrabbit/templates/results.php
+++ bootstrap-idxrabbit/templates/results.php
@@ -5,12 +5,16 @@
 
 get_header(); ?>
 
-<div class="row">
+<div class="row" id="idxrabbit-app">
 	<div class="col-sm-12">
 		<?php 
 			$search_id = get_query_var('search_id');
+			$per_page = get_query_var('per_page');
+			$skip = get_query_var('skip');
+			$orderby = get_query_var('orderby');
+			$detail_page = home_url('property-details') . '/';
 
-			echo do_shortcode("[render template='views/results' search_id='$search_id']");
+			echo do_shortcode("[render template='views/results' search_id='$search_id' per_page='$per_page' skip='$skip' orderby='$orderby' detail_page='$detail_page']");
 		 ?>
 	</div>
 </div>
